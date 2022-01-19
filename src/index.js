2856a13ea6fdc9c71d92eb680fcf9ed9d487d6e0
Hsiao Yao <tomtogo1260@gmail.com>
2022-01-19 08:24:35 +0800

    add three.js & gsap features

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
+/* eslint-disable */
 import './index.css';
 import './css/animate.min.css';
 import './lib/ajax';
 import { NUMBER_MATRIX } from './config.js';
 import * as server from './lib/server.js';
-
 
 import {
   addQipao,
@@ -13,9 +13,10 @@ import {
   resetPrize
 } from "./prizeList";
 
+import gsap from "gsap";
 import * as THREE from 'three';
-import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
-import { CSS3DRenderer, CSS3DObject, CSS3DSprite } from 'three/examples/jsm/renderers/CSS3DRenderer.js';
+import {TrackballControls} from 'three/examples/jsm/controls/TrackballControls';
+import { CSS3DRenderer, CSS3DObject } from 'three/examples/jsm/renderers/CSS3DRenderer.js';
 
 const ROTATE_TIME = 3000;
 const BASE_HEIGHT = 1080;
@@ -68,10 +69,361 @@ function initAll() {
   console.log(document.cookie);
   basicData.users = data;
 
-  // initCards();
-  // animate();
-  // shineCard();
+  TOTAL_CARDS = ROW_COUNT * COLUMN_COUNT;
+
+  initCards();
+  animate();
+  shineCard();
 
   // document.cookie = "username=John Doe";
   // console.log(document.cookie);
+}
+
+function initCards() {
+  let member = basicData.users,
+    showCards = [],
+    length = member.length;
+
+  let isBold = false,
+    showTable = basicData.leftUsers.length === basicData.users.length,
+    index = 0,
+    totalMember = member.length,
+    position = {
+      x: (140 * COLUMN_COUNT - 20) / 2,
+      y: (180 * ROW_COUNT - 20) / 2
+    };
+
+  camera = new THREE.PerspectiveCamera(
+    40,
+    window.innerWidth / window.innerHeight,
+    1,
+    10000
+  );
+  camera.position.z = 3000;
+
+  scene = new THREE.Scene();
+
+  for (let i = 0; i < ROW_COUNT; i++) {
+    for (let j = 0; j < COLUMN_COUNT; j++) {
+      isBold = HIGHLIGHT_CELL.includes(j + "-" + i);
+      var element = createCard(
+        member[index % length],
+        isBold,
+        index,
+        showTable
+      );
+
+      var object = new CSS3DObject(element);
+      object.position.x = Math.random() * 4000 - 2000;
+      object.position.y = Math.random() * 4000 - 2000;
+      object.position.z = Math.random() * 4000 - 2000;
+      scene.add(object);
+      threeDCards.push(object);
+      //
+
+      var object = new THREE.Object3D();
+      object.position.x = j * 140 - position.x;
+      object.position.y = -(i * 180) + position.y;
+      targets.table.push(object);
+      index++;
+    }
+  }
+  // sphere
+
+  var vector = new THREE.Vector3();
+
+  for (var i = 0, l = threeDCards.length; i < l; i++) {
+    var phi = Math.acos(-1 + (2 * i) / l);
+    var theta = Math.sqrt(l * Math.PI) * phi;
+    var object = new THREE.Object3D();
+    object.position.setFromSphericalCoords(800 * Resolution, phi, theta);
+    vector.copy(object.position).multiplyScalar(2);
+    object.lookAt(vector);
+    targets.sphere.push(object);
+  }
+
+  renderer = new CSS3DRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.getElementById("container").appendChild(renderer.domElement);
+
+  //
+
+  controls = new TrackballControls(camera, renderer.domElement);
+  controls.rotateSpeed = 0.5;
+  controls.minDistance = 500;
+  controls.maxDistance = 6000;
+  controls.addEventListener("change", render);
+
+  bindEvent();
+
+  if (showTable) {
+    switchScreen("enter");
+  } else {
+    switchScreen("lottery");
+  }
+}
+
+function animate() {
+  // 让场景通过x轴或者y轴旋转
+  // rotate && (scene.rotation.y += 0.088);
+
+  requestAnimationFrame(animate);
+  // TWEEN.update(); /// TO DO
+  controls.update();
+
+  // 渲染循环
+  // render();
+}
+
+function setLotteryStatus(status = false) {
+  isLotting = status;
+}
+
+/**
+ * 事件绑定
+ */
+ function bindEvent() {
+  document.querySelector("#menu").addEventListener("click", function (e) {
+    e.stopPropagation();
+    // 如果正在抽奖，则禁止一切操作
+    if (isLotting) {
+      addQipao("抽慢一点点～～");
+      return false;
+    }
+
+    let target = e.target.id;
+    switch (target) {
+      // 显示数字墙
+      case "welcome":
+        switchScreen("enter");
+        rotate = false;
+        break;
+      // 进入抽奖
+      case "enter":
+        removeHighlight();
+        addQipao(`马上抽取[${currentPrize.title}],不要走开。`);
+        // rotate = !rotate;
+        rotate = true;
+        switchScreen("lottery");
+        break;
+      // 重置
+      case "reset":
+        let doREset = window.confirm(
+          "是否确认重置数据，重置后，当前已抽的奖项全部清空？"
+        );
+        if (!doREset) {
+          return;
+        }
+        addQipao("重置所有数据，重新抽奖");
+        addHighlight();
+        resetCard();
+        // 重置所有数据
+        currentLuckys = [];
+        basicData.leftUsers = Object.assign([], basicData.users);
+        basicData.luckyUsers = {};
+        currentPrizeIndex = basicData.prizes.length - 1;
+        currentPrize = basicData.prizes[currentPrizeIndex];
+
+        resetPrize(currentPrizeIndex);
+        reset();
+        switchScreen("enter");
+        break;
+      // 抽奖
+      case "lottery":
+        setLotteryStatus(true);
+        // 每次抽奖前先保存上一次的抽奖数据
+        saveData();
+        //更新剩余抽奖数目的数据显示
+        changePrize();
+        resetCard().then(res => {
+          // 抽奖
+          lottery();
+        });
+        addQipao(`正在抽取[${currentPrize.title}],调整好姿势`);
+        break;
+      // 重新抽奖
+      case "reLottery":
+        if (currentLuckys.length === 0) {
+          addQipao(`当前还没有抽奖，无法重新抽取喔~~`);
+          return;
+        }
+        setErrorData(currentLuckys);
+        addQipao(`重新抽取[${currentPrize.title}],做好准备`);
+        setLotteryStatus(true);
+        // 重新抽奖则直接进行抽取，不对上一次的抽奖数据进行保存
+        // 抽奖
+        resetCard().then(res => {
+          // 抽奖
+          lottery();
+        });
+        break;
+      // 导出抽奖结果
+      case "save":
+        saveData().then(res => {
+          resetCard().then(res => {
+            // 将之前的记录置空
+            currentLuckys = [];
+          });
+          exportData();
+          addQipao(`数据已保存到EXCEL中。`);
+        });
+        break;
+    }
+  });
+
+  window.addEventListener("resize", onWindowResize, false);
+}
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  render();
+}
+
+function switchScreen(type) {
+  switch (type) {
+    case "enter":
+      btns.enter.classList.remove("none");
+      btns.lotteryBar.classList.add("none");
+      transform(targets.table, 2000);
+      break;
+    default:
+      btns.enter.classList.add("none");
+      btns.lotteryBar.classList.remove("none");
+      transform(targets.sphere, 2000);
+      break;
+  }
+}
+
+/**
+ * 渲染地球等
+ */
+ function transform(targets, duration) {
+  for (var i = 0; i < threeDCards.length; i++) {
+    var object = threeDCards[i];
+    var target = targets[i];
+
+    gsap.to(object.position, 
+      { 
+        x: target.position.x, 
+        y: target.position.y, 
+        z: target.position.z,
+        duration: Math.random() * duration + duration,
+        ease: "expo.inOut",
+      }
+    );
+
+    gsap.to(object.rotation, 
+      { 
+        x: target.rotation.x, 
+        y: target.rotation.y, 
+        z: target.rotation.z,
+        duration: Math.random() * duration + duration,
+        ease: "expo.inOut",
+      }
+    );
+  }
+
+  gsap.to(this, 
+    { 
+      duration: duration * 2,
+      onUpdate: render,
+    }
+  );
+}
+
+/**
+ * 创建元素
+ */
+ function createElement(css, text) {
+  let dom = document.createElement("div");
+  dom.className = css || "";
+  dom.innerHTML = text || "";
+  return dom;
+}
+
+/**
+ * 创建名牌
+ */
+ function createCard(user, isBold, id, showTable) {
+  var element = createElement();
+  element.id = "card-" + id;
+
+  if (isBold) {
+    element.className = "element lightitem";
+    if (showTable) {
+      element.classList.add("highlight");
+    }
+  } else {
+    element.className = "element";
+    element.style.backgroundColor =
+      "rgba(0,127,127," + (Math.random() * 0.7 + 0.25) + ")";
+  }
+  //添加公司标识
+  element.appendChild(createElement("company", COMPANY));
+
+  element.appendChild(createElement("name", user[1]));
+
+  element.appendChild(createElement("details", user[0] + "<br/>" + user[2]));
+  return element;
+}
+
+function render() {
+  renderer.render(scene, camera);
+}
+
+/**
+ * 随机切换背景和人员信息
+ */
+ function shineCard() {
+  let maxCard = 10,
+    maxUser;
+  let shineCard = 10 + random(maxCard);
+
+  setInterval(() => {
+    // 正在抽奖停止闪烁
+    if (isLotting) {
+      return;
+    }
+    maxUser = basicData.leftUsers.length;
+    for (let i = 0; i < shineCard; i++) {
+      let index = random(maxUser),
+        cardIndex = random(TOTAL_CARDS);
+      // 当前显示的已抽中名单不进行随机切换
+      if (selectedCardIndex.includes(cardIndex)) {
+        continue;
+      }
+      shine(cardIndex);
+      // changeCard(cardIndex, basicData.leftUsers[index]); /// TO DO
+    }
+  }, 500);
+}
+
+/**
+ * 随机抽奖
+ */
+ function random(num) {
+  // Math.floor取到0-num-1之间数字的概率是相等的
+  return Math.floor(Math.random() * num);
+}
+
+/**
+ * 切换名牌背景
+ */
+function shine(cardIndex, color) {
+  let card = threeDCards[cardIndex].element;
+  card.style.backgroundColor =
+    color || "rgba(0,127,127," + (Math.random() * 0.7 + 0.25) + ")";
+}
+
+/**
+ * 切换名牌人员信息
+ */
+ function changeCard(cardIndex, user) {
+  let card = threeDCards[cardIndex].element;
+
+  card.innerHTML = `<div class="company">${COMPANY}</div><div class="name">${
+    user[1]
+  }</div><div class="details">${user[0]}<br/>${user[2] || "PSST"}</div>`;
 }
